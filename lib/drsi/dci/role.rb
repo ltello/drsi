f0eeac3b5269f6cd994312562e6f30a43a226832
Lorenzo Tello <ltello8a@gmail.com>
2014-01-13 17:08:54 +0100

    methods to call a role mate inside a role are actually private

--- lib/drsi/dci/role.rb
+++ lib/drsi/dci/role.rb
@@ -4,14 +4,14 @@ module DCI
     private
 
       def context
-        raise 'This method must be redefined in every module including DCI::Role'
+        raise 'This method must be redefined in every module extending DCI::Role'
       end
 
       # Defines a new private reader instance method for a context mate role, delegating it to the context object.
       def add_role_reader_for!(rolekey)
         return if private_method_defined?(rolekey)
-        private
         define_method(rolekey) {context.send(rolekey)}
+        private rolekey
       end
 
   end
