e03b8784b324db5479adf36e1d5f30a8f3a42fa7
Lorenzo Tello <ltello8a@gmail.com>
2014-01-13 18:29:16 +0100

    test role multiplayers

---
+++ spec/multiplayers_spec.rb
@@ -0,0 +1,134 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'ostruct'
+
+describe 'Role MultiPlayers' do
+
+  context "A role can be played by many objects in the same context at the same time" do
+    before(:all) do
+      class TestingRoleplayersContext < DCI::Context
+        role :role1 do
+          def role1method1; :role1method1_executed end
+          def role1self; self end
+        end
+
+        role :role2 do
+          def role2method1; role1 end
+          def role2self;   self   end
+
+          private
+          def private_role2method2; :private_rolemethod_return_value end
+        end
+
+        def check_role1_identity(obj)
+          [role1 == obj, role1.role1self == obj, role1.respond_to?(:role1method1), obj.respond_to?(:role1method1),
+           role1.role1method1 == :role1method1_executed, obj.role1method1 == :role1method1_executed].uniq == [true]
+        end
+
+        def get_role2_from_role1
+         role1.send(:role2)
+        end
+
+        def multiplayer_dont_get_role
+          [!role2.respond_to?(:role2method1), !role2.private_methods.map(&:to_s).include?('private_role2method2')].uniq == [true]
+        end
+
+        def multiplayer_players_get_their_role?
+          role2players = role2[0..-1]
+          [!role2players.empty?,
+           (role2players.all? {|roleplayer| roleplayer.respond_to?(:role2method1)}),
+           (role2players.all? {|roleplayer| roleplayer.role2method1 == role1}),
+           (role2players.all? {|roleplayer| roleplayer.private_methods.map(&:to_s).include?('private_role2method2')}),
+
+          ].uniq == [true]
+        end
+
+        def check_role2players_identity(*objs)
+          objs.all? do |obj|
+            index = objs.index(obj)
+            [role2 != obj, role2[index] == obj, role2[index].role2self == obj,
+             role2[index].send(:private_role2method2) == :private_rolemethod_return_value,
+             obj.send(:private_role2method2)          == :private_rolemethod_return_value].uniq == [true]
+          end
+        end
+
+        def roles2_external_interfaces_accessible?(*names)
+          names.all? do |name|
+            index = names.index(name)
+            role2[index].name == name
+          end
+        end
+
+        def roles2_have_context_private_access?
+          role2[0..-1].all? do |roleplayer|
+            [!roleplayer.respond_to?(:context), roleplayer.private_methods.map(&:to_s).include?('context'), roleplayer.send(:context) == self].uniq == [true]
+          end
+        end
+
+        def roles2_have_settings_private_access?(settings_copy)
+          role2[0..-1].all? do |roleplayer|
+            [!roleplayer.respond_to?(:settings), roleplayer.private_methods.map(&:to_s).include?('settings'),
+             roleplayer.send(:settings) == settings, roleplayer.send(:settings) == settings_copy,
+             roleplayer.send(:settings, :setting2) == settings_copy[:setting2],
+             roleplayer.send(:settings, :setting1, :setting3) == {:setting1 => settings_copy[:setting1], :setting3 => settings_copy[:setting3]}
+            ].uniq == [true]
+          end
+        end
+
+      end
+
+      @player1, @player2, @player22 = OpenStruct.new(:name => 'player1'), OpenStruct.new(:name => 'player2'), OpenStruct.new(:name => 'player22')
+      @multiplayer = DCI::Multiplayer[@player2, @player22]
+      #puts @multiplayer.instance_variable_get(:@players)
+      @testing_roleplayers_context  = TestingRoleplayersContext.new(:role1    => @player1,
+                                                                    :role2    => @multiplayer,
+                                                                    :setting1 => :one,
+                                                                    :setting2 => :two,
+                                                                    :setting3 => :three)
+    end
+
+    it("For it to work, the developer has to give a rolekey the value of a DCI::Multiplayer instance when instantiating the context...") do
+      @testing_roleplayers_context.get_role2_from_role1.should be(@multiplayer)
+    end
+
+    it("...so now, all the objects wrapped by the multiplayer instance play the role intended") do
+      @testing_roleplayers_context.multiplayer_dont_get_role.should be_true
+      @testing_roleplayers_context.multiplayer_players_get_their_role?.should be_true
+    end
+
+    it("...preserving their identity") do
+      @testing_roleplayers_context.check_role2players_identity(@player2, @player22).should be_true
+    end
+    it("...and therefore, their state and behaviour are accessible inside the context.") do
+      @testing_roleplayers_context.roles2_external_interfaces_accessible?(@player2.name, @player22.name).should be_true
+    end
+
+    it("They have private access to the context.") do
+      @testing_roleplayers_context.roles2_have_context_private_access?.should be_true
+    end
+
+    it("And also private access to extra args received in the instantiation of its context.") do
+      @testing_roleplayers_context.roles2_have_settings_private_access?({:setting1 => :one, :setting2 => :two, :setting3 => :three}).should be_true
+    end
+
+    it("But all these features, are only inside a context. Never out of it!") do
+      @player2.should_not  respond_to(:role1method1)
+      @player22.should_not respond_to(:role1method1)
+      @player2.private_methods.map(&:to_s).should_not include(:private_role2method2)
+      @player22.private_methods.map(&:to_s).should_not include(:private_role2method2)
+      @player2.name.should eq('player2')
+      @player22.name.should eq('player22')
+      @player2.should_not  respond_to(:role1)
+      @player22.should_not respond_to(:role1)
+      @player2.should_not  respond_to(:context)
+      @player22.should_not respond_to(:context)
+      @player2.should_not  respond_to(:settings)
+      @player22.should_not respond_to(:settings)
+      (@player2.private_methods.map(&:to_s) &  ['role1', 'context', 'settings']).should be_empty
+      (@player22.private_methods.map(&:to_s) & ['role1', 'context', 'settings']).should be_empty
+    end
+
+
+
+  end
+
+end
